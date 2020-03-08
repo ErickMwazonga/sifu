0e4db72c9d82ef65e3bf384d4835610120919b2b
Erick Mwazonga <erickmwazonga@gmail.com>
2020-03-08 09:27:58 +0300

    Add array functions

--- arrays/duplicates/get_duplicates.py
+++ arrays/duplicates/get_duplicates.py
@@ -27,7 +27,7 @@ def get_duplicates(arr):
         else:
             counts[value] = 1
 
-get_duplicates([1,2, 2,2,3])
+get_duplicates([1,2, 2, 2, 3])
 # get_duplicates([1,2,1,2])
 
 
---
+++ arrays/smallest_number_than_current.py
@@ -0,0 +1,24 @@
+"""
+https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/
+Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it.
+That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+
+Return the answer in an array.
+Example 1:
+
+Input: nums = [8,1,2,2,3]
+Output: [4,0,1,1,3]
+"""
+
+def smallerNumbersThanCurrent(nums):
+    sorted_nums = sorted(nums)
+    mappings = {}
+    
+    for k, v in enumerate(sorted_nums):
+        if v not in mappings:
+            mappings[v] = k
+    return [mappings[x] for x in nums]
+
+
+nums = [8, 1, 2, 2, 3] # Output: [4,0,1,1,3]
+print(smallerNumbersThanCurrent(nums))
