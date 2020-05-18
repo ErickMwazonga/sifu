137b09daa2f961fd683684380d264e7b1c7a770e
Erick Mwazonga <erickmwazonga@gmail.com>
2020-05-18 23:12:21 +0300

    Quicksort

--- heap/kth_largest_elem.py
+++ heap/kth_largest_elem.py
@@ -1,3 +1,19 @@
+'''
+Find the kth largest element in an unsorted array.
+Note that it is the kth largest element in the sorted order,
+not the kth distinct element.
+
+Examples
+Input: [3,2,1,5,6,4] and k = 2
+Output: 5
+
+Input: [3,2,3,1,2,4,5,5,6] and k = 4
+Output: 4
+
+Note:
+You may assume k is always valid, 1 ≤ k ≤ array's length.
+'''
+
 import heapq
 
 
---
+++ sorting/quick_sort.py
@@ -0,0 +1,41 @@
+'''
+https://www.programiz.com/dsa/quick-sort
+
+Worst Case Complexity -> O(n^2)
+Pivot element picked is either the greatest or the smallest element.
+
+Best Case Complexity -> O(n*log n)
+Pivot element is always the middle element or near to the middle element.
+
+Average Case Complexity [Big-theta]: O(n*log n)
+It occurs when the above conditions do not occur.
+
+The space complexity -> O(log n).
+'''
+
+
+def partition(A, low, high):
+    pivot = A[high]
+    i = low
+
+    for j in range(low, high):  # Upto the 2nd last element
+        if A[j] <= pivot:
+            A[i], A[j] = A[j], A[i]
+            i += 1
+
+    A[i], A[high] = A[high], A[i]
+    return i
+
+
+def quickSort(A, low, high):
+    if low < high:
+        pi = partition(A, low, high)
+        quickSort(A, low, pi-1)
+        quickSort(A, pi+1, high)
+
+
+data = [8, 7, 2, 1, 0, 9, 6]
+size = len(data)
+quickSort(data, 0, size - 1)
+
+assert data == [0, 1, 2, 6, 7, 8, 9]
