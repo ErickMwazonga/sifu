cb88343677b078d3eda917b2dc47ab8f6a1983ea
Erick Mwazonga <erickmwazonga@gmail.com>
2020-12-03 16:58:28 +0300

    Shuffle array

---
+++ arrays/general/shuffle_array.py
@@ -0,0 +1,37 @@
+'''
+384. Shuffle an Array
+https://leetcode.com/problems/shuffle-an-array/
+
+Given an integer array nums, design an algorithm to randomly shuffle the array.
+Implement the Solution class:
+Solution(int[] nums) Initializes the object with the integer array nums.
+int[] reset() Resets the array to its original configuration and returns it.
+int[] shuffle() Returns a random shuffling of the array.
+'''
+
+class Solution:
+
+    def __init__(self, A: List[int]):
+        self.A = A
+        self.original_nums = A[:]
+
+    def reset(self) -> List[int]:
+        """
+        Resets the array to its original configuration and return it.
+        """
+        return self.original_nums
+        
+
+    def shuffle(self) -> List[int]:
+        """
+        Returns a random shuffling of the array.
+        Inspired by - https://www.youtube.com/watch?v=4zx5bM2OcvA
+        """
+        
+        last_index = len(self.A) - 1
+        while last_index > 0:
+            rand_index = random.randint(0, last_index)
+            self.A[last_index], self.A[rand_index] = self.A[rand_index], self.A[last_index]
+            last_index -= 1
+        
+        return self.A
---
+++ trees/cousins.py
@@ -0,0 +1,34 @@
+'''
+993. Cousins in Binary Tree
+https://leetcode.com/problems/cousins-in-binary-tree/
+
+In a binary tree, the root node is at depth 0, and children of each depth k node are at depth k+1.
+Two nodes of a binary tree are cousins if they have the same depth, but have different parents.
+We are given the root of a binary tree with unique values, and the values x and y of two different nodes in the tree.
+Return true if and only if the nodes corresponding to the values x and y are cousins.
+'''
+
+def isCousins(root: TreeNode, x: int, y: int) -> bool:
+    res = [] # store (parent, depth) tuple
+    
+    # bfs
+    queue = [(root, None, 0)]      
+    while queue:
+        # minor optimization to stop early if both targets found
+        if len(res) == 2:
+            break
+        node, parent, depth = queue.pop(0)
+        
+        # if target found
+        if node.val == x or node.val == y:
+            res.append((parent, depth))
+        if node.left:
+            queue.append((node.left, node, depth + 1))
+        if node.right:
+            queue.append((node.right, node, depth + 1))
+
+    # unpack two nodes
+    node_x, node_y = res
+    
+    # compare and decide whether two nodes are cousins		
+    return node_x[0] != node_y[0] and node_x[1] == node_y[1]
