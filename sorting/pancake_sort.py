e34317359d7813e354a06be96ee3aebf25bd4c66
Erick Mwazonga <erickmwazonga@gmail.com>
2020-12-07 16:48:29 +0300

    After watching mock interviews

---
+++ sorting/pancake_sort.py
@@ -0,0 +1,40 @@
+'''
+969. Pancake Sorting
+https://leetcode.com/problems/pancake-sorting/
+
+Given an array of integers arr, sort the array by performing a series of pancake flips.
+In one pancake flip we do the following steps:
+
+Choose an integer k where 1 <= k <= arr.length.
+Reverse the sub-array arr[1...k].
+For example, if arr = [3,2,1,4] and we performed a pancake flip choosing k = 3, we reverse the sub-array [3,2,1],
+so arr = [1,2,3,4] after the pancake flip at k = 3.
+
+Return the k-values corresponding to a sequence of pancake flips that sort arr. Any valid answer that sorts
+the array within 10 * arr.length flips will be judged as correct.
+'''
+
+def flip(arr, i):
+    arr[:i+1] = reversed(arr[:i+1])
+
+def findMaxUpTo(arr, i):
+    val = max(arr[:i+1])
+    return arr.index(val)
+
+def pancake_sort(arr):
+    # start from the array and one by one reduce the current size
+    curr_size = len(arr) - 1
+    # find the index of the maxmium element inside the arr[0..curr_size -1]
+    while curr_size > 0:
+        mi = findMaxUpTo(arr, curr_size)
+        if mi != curr_size:
+            flip(arr, mi)
+            # once I flip it
+            # now move the maximum number to the end by reversing current array
+            flip(arr, curr_size)
+        curr_size -= 1
+    return arr
+
+
+# print(pancake_sort([3,2,1,4]))
+assert pancake_sort([3, 2, 1, 4]) == [1, 2, 3, 4]
---
+++ strings/longest_common_prefix.py
@@ -0,0 +1,38 @@
+'''
+14. Longest Common Prefix
+Write a function to find the longest common prefix string amongst an array of strings.
+If there is no common prefix, return an empty string "".
+
+Example 1:
+Input: strs = ["flower","flow","flight"] -> "fl"
+
+Example 2:
+Input: strs = ["dog","racecar","car"] -> ""
+Explanation: There is no common prefix among the input strings.
+'''
+
+def longestCommonPrefix(strs) -> str:
+    l = list(zip(*strs))
+    prefix = ''
+    
+    for i in l:
+        if len(set(i)) == 1:
+            prefix += i[0]
+        else:
+            break
+            
+    return prefix
+
+
+def longestCommonPrefix(strs) -> str:
+    if not strs or len(strs) == 0:
+        return ""
+
+    for i in range(len(strs[0])): 
+        c = strs[0][i]
+
+        for j in range(1, len(strs)):
+            if i == len(strs[j]) or strs[j][i] != c:
+                return strs[0][:i]
+                
+    return strs[0] if strs else ""
