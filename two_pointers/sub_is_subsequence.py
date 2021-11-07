8af68f48eb88bb2237a364d72e4747cf7863b0c3
Erick Mwazonga <emwazonga@microsoft.com>
2021-11-08 01:35:38 +0300

    Validate subsequences

---
+++ two_pointers/sub_is_subsequence.py
@@ -0,0 +1,32 @@
+'''
+392. Is Subsequence
+https://leetcode.com/problems/is-subsequence/
+
+Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none)
+of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+Example 1:
+Input: s = "abc", t = "ahbgdc"
+Output: true
+
+Example 2:
+Input: s = "axc", t = "ahbgdc"
+Output: false
+'''
+
+
+def isSubsequence(s: str, t: str) -> bool:
+    sIdx, tIdx = 0, 0
+
+    while sIdx < len(s) and tIdx < len(t):
+        if s[sIdx] == t[tIdx]:
+            sIdx += 1
+
+        tIdx += 1
+
+    return sIdx == len(s)
+
+
+assert isSubsequence("abc", "ahbgdc") == True
+assert isSubsequence("axc", "ahbgdc") == False
---
+++ two_pointers/sub_validate_sequence.py
@@ -0,0 +1,29 @@
+'''
+Validate Sequence
+Given two non-empty arrays of integers, write a function to determine whether the second array is a subsequence of the first one.
+A subsequene of the array is a set of numbers that arent necessarily adjascent in rthe array but are in the same order as they appear in the array. 
+For instance, the numbers [1,3,4] form a subsequence in the array [1,2,3,4] and so do the numbers [2,4]. 
+Note that a single number in an array and the array itself are both valid subsequence of the array.
+
+Sample Input
+array = [5,1,22,25,6,-1,8,10]
+sequence = [1,6,-1,10]
+'''
+
+
+def isValidSubsequence(array, sequence):
+    arrayIdx, seqIdx = 0, 0
+
+    while arrayIdx < len(array) and seqIdx < len(sequence):
+        if array[arrayIdx] == sequence[seqIdx]:
+            seqIdx += 1
+
+        arrayIdx += 1
+
+    return seqIdx == len(sequence)
+
+
+array = [5, 1, 22, 25, 6, -1, 8, 10]
+sequence = [1, 6, -1, 10]
+
+assert isValidSubsequence(array, sequence) == True
