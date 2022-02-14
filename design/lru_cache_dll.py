7d51c97d8c733729942bbd6314a5de3ed914ea30
Erick Mwazonga <emwazonga@microsoft.com>
2022-02-15 01:56:21 +0300

    Add steps for the LRUCache Implementation

--- design/lru_cache_dll.py
+++ design/lru_cache_dll.py
@@ -1,6 +1,7 @@
 '''
 146. LRU Cache
 https://leetcode.com/problems/lru-cache/
+Credit: https://www.youtube.com/watch?v=7ABFKPK2hD4
 
 Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
 
@@ -27,12 +28,14 @@ class LRUCache:
     def __init__(self, capacity: int):
         self.capacity = capacity
         self.cache = {}
+
         self.left = Node(-1, -1)
         self.right = Node(-1, -1)
+
         self.left.next = self.right
         self.right.prev = self.left
 
-    def add_node(self, node):
+    def insert(self, node):
         prev = self.right.prev
         nxt = self.right
 
@@ -42,31 +45,52 @@ class LRUCache:
         node.next = nxt
         node.prev = prev
 
-    def remove_node(self, node):
+    def remove(self, node):
         prev, nxt = node.prev, node.next
         prev.next = nxt
         nxt.prev = prev
 
     def get(self, key: int) -> int:
+        '''
+        1. Check if key exists in cache
+        2. Get node in cache(hashmap), given the key
+        3. It needs to be the most recently used now 
+            a. Remove it 
+            b. Insert it again
+        '''
+
         if key not in self.cache:
             return -1
 
         node = self.cache[key]
-        self.remove_node(node)
-        self.add_node(node)
+        self.remove(node)
+        self.insert(node)
 
         return node.val
 
     def put(self, key: int, value: int) -> None:
+        '''
+        1. Check if key exists in cache
+            a. Remove it 
+            b. Insert it again
+        2. Else:
+            a. Create a new node and it in cache
+            b. Insert it in dll
+        3. Check if the cache is full
+            a. Remove the least used node from dll
+            b. Remoe from cache
+        '''
+
         if key in self.cache:
             node = self.cache[key]
-            self.remove_node(node)
-
-        node = Node(key, value)
-        self.add_node(node)
-        self.cache[key] = node
+            self.remove(node)
+            self.insert(node)
+        else:
+            node = Node(key, value)
+            self.cache[key] = node
+            self.insert(node)
 
         if len(self.cache) > self.capacity:
-            lru = self.left.next
-            self.remove_node(lru)
-            self.cache.pop(lru.key)
+            evicted = self.left.next
+            self.remove(evicted)
+            self.cache.pop(evicted.key)  # del self.cache[evicted.key]
