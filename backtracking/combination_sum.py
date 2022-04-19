cd3b093f287f9a36cabc26e9cbfc6afc95c15bd0
Erick Mwazonga <emwazonga@microsoft.com>
2022-04-19 18:12:56 +0300

    Re-arrangements

--- backtracking/combination_sum.py
+++ backtracking/combination_sum.py
@@ -20,3 +20,47 @@ Example 2:
 Input: candidates = [2,3,5], target = 8
 Output: [[2,2,2,2],[2,3,3],[3,5]]
 '''
+
+
+class Solution:
+    def combinationSum(self, candidates, target):
+        candidates.sort()
+        res = []
+
+        self.helper(candidates, target, res, cur_subset=[], index=0)
+
+        return res
+
+    def helper(self, candidates, remain, res, cur_subset, index):
+        if remain == 0:
+            return res.append(cur_subset)
+
+        for i in range(index, len(candidates)):
+            if candidates[i] > remain:
+                return
+
+            cur_subset.append(candidates[i])
+            self.helper(candidates, remain - candidates[i], res, cur_subset, i)
+            cur_subset.pop()
+
+
+class Solution2:
+    def combinationSum(self, candidates, target):
+        res = []
+        self.helper(candidates, target, res, total=0, index=0, subset=[])
+
+        return res
+
+    def helper(self, cands, target, res, total, index, subset):
+        if total == target:
+            res.append(subset[:])
+            return
+
+        if index >= len(cands) or total > target:
+            return
+
+        subset.append(cands[index])
+        self.helper(cands, target, res, total + cands[index], index, subset)
+        subset.pop()
+
+        self.helper(cands, target, res, total, index + 1, subset)
