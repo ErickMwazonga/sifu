d3fb21ece8d3783fdaea932c651d0ae4961c2a8b
Erick Mwazonga <erickmwazonga@gmail.com>
2022-06-07 19:26:59 +0300

    Remove unnecessary if else conditions

--- recursion/learning/check_for_next_duplicate.py
+++ recursion/learning/check_for_next_duplicate.py
@@ -12,7 +12,8 @@ Examples
 def hasAdjacentDuplicates(s, i=0):
     if i >= len(s) - 1:
         return False
-    elif s[i] == s[i+1]:
+    
+    if s[i] == s[i+1]:
         return True
-    else:
-        return hasAdjacentDuplicates(s, i+1)
+    
+    return hasAdjacentDuplicates(s, i+1)
