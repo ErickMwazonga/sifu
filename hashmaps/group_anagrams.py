448a5af75ff20a2347152c392214bc6620356fe4
Erick Mwazonga <emwazonga@microsoft.com>
2022-07-06 16:41:18 +0300

    Refactor group anagrams

--- hashmaps/group_anagrams.py
+++ hashmaps/group_anagrams.py
@@ -6,7 +6,7 @@ Given an array of strings strs, group the anagrams together. You can return the 
 An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
 typically using all the original letters exactly once.
 
-Example 1:
+Example:
 Input: strs = ['eat', 'tea', 'tan', 'ate', 'nat', 'bat']
 Output: [['bat'], ['nat', 'tan'], ['ate', 'eat', 'tea']]
 
