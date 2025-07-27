245a999a966045c217a8c74f7f0e2e0bd88160a5
Erick Mwazonga <erimwazo@amazon.com>
2025-07-28 00:10:50 +0100

    palindrome number

---
+++  c++/palindromeNumber.cpp
@@ -0,0 +1,20 @@
+// 9. Palindrome Number - https://leetcode.com/problems/palindrome-number/description/
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        int original = x;
+        long long reversedNum = 0;
+        while (x > 0) {
+            int rem = x % 10;
+            reversedNum = reversedNum * 10 + rem;
+            x /= 10; 
+        }
+
+        return original == reversedNum;
+    }
+};
